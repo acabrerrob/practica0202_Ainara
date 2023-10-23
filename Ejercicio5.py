af52394455fe2d3d71d90373e3dde39f86f7053a
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-23 20:27:50 +0200

    Update Ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -2,3 +2,6 @@
 # de acuerdo al sexo y el nombre. Gryffindor está formado por las mujeres con un nombre
 # anterior a la M y los hombres con un nombre posterior a la N y Slytheryn por el resto.
 # Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
+name = input ('Introduzca su nombre: ')
+gender = input ('Introduzca su sexo: ')
+
