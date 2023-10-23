ff0860b31dd995db00f8ac6a67bcaf731c553aa1
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-23 19:43:58 +0200

    Update Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+passwd = 'contraseña'
+
+userPasswd = input ('Introduzca la contraseña ') .lower()
+
+if passwd == userPasswd :
+    print ('La contraseña intrducida es correcta ')
+
+else :
+    print ('La contraseña es incorrecta, por favor introduzca la contraseña. ')
+
