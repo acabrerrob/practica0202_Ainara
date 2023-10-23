5d4bc3e0f2c702724f4ea1bc4947ce479e3a8bc6
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-23 20:00:22 +0200

    Update Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+nOne = float (input ('Introduzca un número: '))
+nTwo = float (input ('Introduzca otro número: '))
+
+if nTwo == 0:
+     
+     print('Error')
+
+else :
+    nTwo != 0 
+
+    print (nOne / nTwo)
