0811b1f4bf7c97cd2e2482d56e45d2d50cb87a02
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-25 20:05:35 +0200

    Update Ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,7 +1,3 @@
-# Los alumnos de Hogwarts se han dividido en dos casas, Gryffindor y Slytherin,
-# de acuerdo al sexo y el nombre. Gryffindor está formado por las mujeres con un nombre
-# anterior a la M y los hombres con un nombre posterior a la N y Slytheryn por el resto.
-# Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
 name = input ('Introduzca su nombre: ')
 gender = input ('Indique si usted es mujer u hombre: ')
 
