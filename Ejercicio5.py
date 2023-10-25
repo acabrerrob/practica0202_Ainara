a477179cf6be3ac215d2c266b08c2ae6b43a759a
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-25 19:48:30 +0200

    Update Ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -3,10 +3,18 @@
 # anterior a la M y los hombres con un nombre posterior a la N y Slytheryn por el resto.
 # Escribir un programa que pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
 name = input ('Introduzca su nombre: ')
-gender = input ('Introduzca su sexo: ')
+gender = input ('Indique si usted es mujer u hombre: ')
 
 uName = name.upper()
+uGender = gender.upper()
 
-
-if uName[:1] == 'A' or  uName[:1] == 'B' or uName[:1] == 'C':
-    print (uName[:1])
+if uName[:1] in {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' 'I', 'J', 'K', 'L', 'M'}:
+    if uGender == 'MUJER':
+        print('Griffindor')
+    else :
+        print('Slythering')
+elif uName[:1] in {'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}  :
+    if uGender == 'HOMBRE' :
+        print ('Grifindor')
+    else :
+        print ('Slythering')
