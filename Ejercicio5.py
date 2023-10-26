c3cb9e05eca45d74c5f28b58fdefb04fc7ccbfe6
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-26 15:29:35 +0200

    Update Ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -5,14 +5,14 @@ uName = name.upper()
 uGender = gender.upper()
 
 if uName[:1] in {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' 'I', 'J', 'K', 'L', 'M'}:
-    if uGender == 'MUJER':
-        print('Griffindor')
-    else :
-        print('Slythering')
+     if uGender == 'MUJER':
+         print('Griffindor')
+     else :
+         print('Slythering')
 elif uName[:1] in {'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}  :
-    if uGender == 'HOMBRE' :
-        print ('Grifindor')
-    else :
-        print ('Slythering')
+     if uGender == 'HOMBRE' :
+         print ('Grifindor')
+     else :
+         print ('Slythering')
 else :
-    print('Algún dato de los introducidos no es válido, vuelva a introducirlos. ')
+     print('Algún dato de los introducidos no es válido, vuelva a introducirlos. ')
