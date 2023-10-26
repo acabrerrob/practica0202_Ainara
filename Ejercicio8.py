7bda51a992afd2e4ed89a7830d757f119c2a5719
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-26 16:23:02 +0200

    Update Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,90 @@
+# Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+print ('Tabla del 1')
+
+
+counter = 0
+
+while counter <= 10 :
+    print (1*counter)
+    counter +=1
+
+
+print ('Tabla del 2')
+
+counter = 0
+
+while counter <= 10 :
+    print (2*counter)
+    counter +=1
+
+
+print ('Tabla del 3')
+
+counter = 0
+
+while counter <= 10 :
+    print (3*counter)
+    counter +=1
+
+
+print ('Tabla del 4')
+
+counter = 0
+
+while counter <= 10 :
+    print (4*counter)
+    counter +=1
+
+
+print ('Tabla del 5')
+
+counter = 0
+
+while counter <= 10 :
+    print (5*counter)
+    counter +=1
+
+
+print ('Tabla del 6')
+
+counter = 0
+
+while counter <= 10 :
+    print (6*counter)
+    counter +=1
+
+
+print ('Tabla del 7')
+
+counter = 0
+
+while counter <= 10 :
+    print (7*counter)
+    counter +=1
+
+
+print ('Tabla del 8')
+
+counter = 0
+
+while counter <= 10 :
+    print (8*counter)
+    counter +=1
+
+
+print ('Tabla del 9')
+
+counter = 0
+
+while counter <= 10 :
+    print (9*counter)
+    counter +=1
+
+
+print ('Tabla del 10')
+
+counter = 0
+
+while counter <= 10 :
+    print (10*counter)
+    counter +=1
