acc57663a691743dbc7a06865aab6de6a1165ba7
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-27 22:25:03 +0200

    Update Ejercicio12.py

---
+++ Ejercicio12.py
@@ -0,0 +1,7 @@
+sentence = input('Introduzca una frase: ')
+character = input('Introduzca una letra: ')
+
+sentenceUp = sentence.upper()
+characterUp = character.upper()
+
+print(sentenceUp.count(characterUp))
